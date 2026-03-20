2e32488c0c3ad1df522f652ba04463a6e3a5651e
Dooheli <julespenas08@gmail.com>
2026-03-20 19:42:07 +0100

    boss et carte
    fonction boss pour le blob pour qu'il puisse changer de sprite et de stats
    Rajout de cartes et d'un mob

--- sources/fonctions_boss.py
+++ sources/fonctions_boss.py
@@ -9,6 +9,33 @@ class BossInterfaceMethodes:
     # default_factory est appelé et sa valeur de retour va initialiser l'attribut
     attributs_supplementaires : dict[str, Any] = field(default_factory=lambda: {})
 
+
+def roi_blob_nouveau_tour(monstre: Monstre, attributs: dict[str, Any]) -> None:
+    if "sprite physique" not in attributs.keys():
+        sprite_physique = pygame.image.load("data/img/boss/Roi blob ph.png").convert_alpha()
+        sprite_magie = pygame.image.load("data/img/boss/Roi blob mg.png").convert_alpha()
+
+        w, h = sprite_physique.get_size()
+        facteur = 6
+        attributs["sprite physique"] = pygame.transform.scale(sprite_physique, (int(w * facteur), int(h * facteur)))
+
+        w, h = sprite_magie.get_size()
+        attributs["sprite magie"] = pygame.transform.scale(sprite_magie, (int(w * facteur), int(h * facteur)))
+    
+    if Jeu.nb_tours_combat % 3 == 0:
+        monstre._stats.defense_magique = 50
+        monstre._stats.defense = 25
+        monstre._stats.magie = 30
+        monstre._stats.force = 50
+        monstre._sprite = attributs["sprite physique"]
+    else :
+        monstre._stats.defense_magique = 25
+        monstre._stats.defense = 50
+        monstre._stats.magie = 50
+        monstre._stats.force = 30
+        monstre._sprite = attributs["sprite magie"]
+
+
 callbacks : dict[str, BossInterfaceMethodes] = {
-    "Roi Blob": BossInterfaceMethodes(),
+    "Roi Blob": BossInterfaceMethodes(roi_blob_nouveau_tour),
 }
