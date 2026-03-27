e60b9594ffc5a50fec5d4ea47d677a87e5f5aa87
Nils Adekambi <lecodeurenretard@gmail.com>
2026-03-27 17:40:07 +0100

    Réparation rapide

--- sources/fonctions_cartes.py
+++ sources/fonctions_cartes.py
@@ -4,21 +4,14 @@ Contient toutes les fonctions utilisées par les cartes ainsi que callbacks[] qu
 projet : L'ascension de Esquimot
 auteur : Dooheli, Lecodeurenretard, hibou509
 """
-from Attaque import *
+from Carte import *
 
-#reprend le principe de BossInterfaceMethodes mais pour les cartes.
 
-@dataclass
-class CarteInterfaceMethodes:
-    appliquer : None|Callable[[Attaque, dict[str, Any]], None] = None
-    
-    attributs_supplementaires : dict[str, Any] = field(default_factory=dict)
-
-def quitte_ou_double_aplliquer(attaque : Attaque, attributs : dict[str, Any]) -> None:
+def quitte_ou_double_aplliquer(self : Carte, attr : dict[str, Any]) -> None:
     if random.randint(0, 1) <= 0.5:
-        attaque.cible.recoit_degats(40, attaque)
+        self._attaque.cible.recoit_degats(40, self)
     else:
-        attaque.lanceur.recoit_degats(40, attaque)
+        self._attaque.lanceur.recoit_degats(40, self)
 
 callbacks : dict[str, CarteInterfaceMethodes]  = {
     "quitte ou double" : CarteInterfaceMethodes(quitte_ou_double_aplliquer)
